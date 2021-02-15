053102cdb72683c583f92b245e49503ac8bf1043
gadhagod <69025547+gadhagod@users.noreply.github.com>
2021-02-15 15:44:20 -0800

    Add search bar

---
+++ config.js
@@ -0,0 +1,15 @@
+window.$docsify = {
+    name: 'Harker Hackers',
+    repo: '',
+    loadSidebar: true,
+    coverpage: true,
+    search: 'auto',
+    search: {
+        maxAge: 86400000,
+        paths: 'auto',
+        placeholder: 'Search',
+        noData: 'No Results!',
+        depth: 10,
+        hideOtherSidebarContent: false,
+    }
+}
